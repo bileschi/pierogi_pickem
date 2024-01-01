d655ee634f3b5c7fdb9f0e2a768126a9476262be
Stanley Bileschi <bileschi@google.com>
2024-01-01 00:40:32 -0500

    integrate ESPN picks

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ WEEK_KEY = 'week'
 HOME_KEY = 'home_team'
 AWAY_KEY = 'away_team'
 GAME_ID_KEY = 'game_id'
-CORRECT_OUTCOME_KEY = 'correct_outcome_key'
-POSSIBLE_OUTCOMES_KEY = 'possible_outcomes'
+CORRECT_OUTCOME_ABBREV_KEY = 'correct_outcome_key'
+INCORRECT_OUTCOME_ABBREV_KEY = 'incorrect_outcome_key'
 PROP_NAME_KEY = 'prop_name'
 PROPOSITION_ID_KEY = 'proposition_id'
 LINE_KEY = 'home_line'
@@ -22,15 +22,17 @@ SMB_PICK_KEY = 'smb_pick'
 SLB_PICK_KEY = 'slb_pick'
 SUE_PICK_KEY = 'sue_pick'
 JEAN_PICK_KEY = 'jean_pick'
+MORGAN_PICK_KEY = 'morgan_pick'
 OUTCOME_ID_KEY = 'outcome_id'
 RESULT_KEY = 'result'
 
-game_col_keys = (
+GAME_COL_KEYS = (
   WEEK_KEY,  # get_game_scores
   HOME_KEY,  # get_game_scores
   AWAY_KEY,  # get_game_scores
   GAME_ID_KEY,  # get_game_scores
   PROPOSITION_ID_KEY,  # get_propositions
+  CORRECT_OUTCOME_ABBREV_KEY, # get_propositions
   HOME_SCORE_KEY,  # get_game_scores
   AWAY_SCORE_KEY,  # get_game_scores
   LINE_KEY,  # get_propositions
@@ -38,24 +40,30 @@ game_col_keys = (
   SLB_PICK_KEY,
   SUE_PICK_KEY,
   JEAN_PICK_KEY,
+  MORGAN_PICK_KEY,
 )
 
-prop_col_keys = (
+PROP_COL_KEYS = (
   PROPOSITION_ID_KEY,
   LINE_KEY,
-  CORRECT_OUTCOME_KEY,
-  POSSIBLE_OUTCOMES_KEY,
+  CORRECT_OUTCOME_ABBREV_KEY,
+  INCORRECT_OUTCOME_ABBREV_KEY, # get_propositions
   PROP_NAME_KEY, 
   GAME_ID_KEY
 )
 
-picks_col_keys = (
+PICKS_COL_KEYS = (
   PROPOSITION_ID_KEY,
   OUTCOME_ID_KEY,
   RESULT_KEY,
 )
 
-
+PLAYER_IDS = {
+  SMB_PICK_KEY: '72520e10-4b6e-11ee-a4c8-27e5ad8f3bbd',
+  SLB_PICK_KEY: '385fbc90-4b71-11ee-85db-77e9d8fab8a0',
+  SUE_PICK_KEY: '4d1a4380-4bfd-11ee-b3fb-11aa5673265b',
+  JEAN_PICK_KEY: '5d03d350-4c3a-11ee-8382-91995701af39',
+}
 
 def parse_score_text(score_text):
   # expect text like "ATL 25, GB 24" or "BUF 38, LV 10"
@@ -65,7 +73,7 @@ def parse_score_text(score_text):
   return(away_score, home_score)
 
 def game_as_str(game_dict):
-  return(','.join([str(game_dict[k]) for k in game_col_keys]))
+  return(','.join([str(game_dict[k]) for k in GAME_COL_KEYS]))
 
 
 def get_game_scores():
@@ -95,7 +103,7 @@ def get_game_scores():
         'tr',
         class_='Table__TR Table__TR--sm Table__even')
       for i_r, row in enumerate(rows[:]):
-        game = {k: '' for k in game_col_keys}
+        game = {k: '' for k in GAME_COL_KEYS}
         game[WEEK_KEY] = week
         # Get the teams
         teams = row.find_all('span', class_='Table__Team')
@@ -139,17 +147,20 @@ def get_propositions():
       proposition = {
           PROPOSITION_ID_KEY: None,
           LINE_KEY: None,
-          CORRECT_OUTCOME_KEY: None,
-          POSSIBLE_OUTCOMES_KEY: None,
+          CORRECT_OUTCOME_ABBREV_KEY: None,
+          INCORRECT_OUTCOME_ABBREV_KEY: None,
           PROP_NAME_KEY: None, 
           GAME_ID_KEY: None
       }
       if len(one_json_prop['correctOutcomes']) == 1:
-        proposition[CORRECT_OUTCOME_KEY] = one_json_prop['correctOutcomes'][0]
-      possible_outcomes = {}
+        correct_outcome_id = one_json_prop['correctOutcomes'][0]
+      else:
+        correct_outcome_id = None
       for possible_outcome in one_json_prop['possibleOutcomes']:
-        possible_outcomes[possible_outcome['id']] = possible_outcome['abbrev']
-      proposition[POSSIBLE_OUTCOMES_KEY] = possible_outcomes
+        if possible_outcome['id'] == correct_outcome_id:
+          proposition[CORRECT_OUTCOME_ABBREV_KEY] = possible_outcome['abbrev']
+        else:
+          proposition[INCORRECT_OUTCOME_ABBREV_KEY] = possible_outcome['abbrev']
       proposition[PROPOSITION_ID_KEY] = one_json_prop['id']
       proposition[PROP_NAME_KEY] = one_json_prop['name']
       if 'spread' in one_json_prop:
@@ -160,59 +171,88 @@ def get_propositions():
       propositions.append(proposition)
     return(propositions)
 
-def write_games_csv(games):
-  # Write CSV with one row per game.  Include keys as above.
-  with open('games.csv', 'w', newline='') as csvfile:
-    gamewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    gamewriter.writerow(game_col_keys)
-    for game in games:
-      gamewriter.writerow([game[k] for k in game_col_keys])
-
-def write_propositions_csv(propositions):
-  with open('propositions.csv', 'w', newline='') as csvfile:
-    propwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    propwriter.writerow(prop_col_keys)
-    for prop in propositions:
-      propwriter.writerow([prop[k] for k in prop_col_keys])
-
-def write_picks_csv(picks, filename):
-  with open(filename, 'w', newline='') as csvfile:
-    pickwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    pickwriter.writerow(picks_col_keys)
-    for pick in picks:
-      pickwriter.writerow([pick[k] for k in picks_col_keys])
-
-def get_smb_picks():
-  smb_picks = []
-  smb_picks_url = (
-    f'https://gambit-api.fantasy.espn.com/apis/v1/challenges/230/entries/72520e10-4b6e-11ee-a4c8-27e5ad8f3bbd?platform=chui&view=chui_default'
+def get_picks(pick_id):
+  picks = []
+  picks_url = (
+    f'https://gambit-api.fantasy.espn.com/apis/v1/challenges/230/entries/{pick_id}?platform=chui&view=chui_default'
   )
   response = requests.get(
-    smb_picks_url,
+    picks_url,
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
   soup = BeautifulSoup(response.content, 'html.parser')
-  smb_picks_json = json.loads(soup.text)
-  for one_json_pick in smb_picks_json['picks']:
-    smb_pick = {
+  picks_json = json.loads(soup.text)
+  for one_json_pick in picks_json['picks']:
+    pick = {
       PROPOSITION_ID_KEY: None,
       OUTCOME_ID_KEY: None,
       RESULT_KEY: None
     }
     if len(one_json_pick['outcomesPicked']) == 1:
       one_outcome = one_json_pick['outcomesPicked'][0]
-      smb_pick[OUTCOME_ID_KEY] = one_outcome['outcomeId']
-      smb_pick[RESULT_KEY] = one_outcome['result']
+      pick[OUTCOME_ID_KEY] = one_outcome['outcomeId']
+      pick[RESULT_KEY] = one_outcome['result']
     # Otherwise no pick.
     # TODO: handle multiple picks.  - is that even possible?
-    smb_pick[PROPOSITION_ID_KEY] = one_json_pick['propositionId']
-    smb_picks.append(smb_pick)
-  return(smb_picks)
+    pick[PROPOSITION_ID_KEY] = one_json_pick['propositionId']
+    picks.append(pick)
+  return(picks)
+
+def write_games_csv(games, filename):
+  # Write CSV with one row per game.  Include keys as above.
+  with open(filename, 'w', newline='') as csvfile:
+    gamewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    gamewriter.writerow(GAME_COL_KEYS)
+    for game in games:
+      gamewriter.writerow([game[k] for k in GAME_COL_KEYS])
+
+def write_propositions_csv(propositions):
+  with open('propositions.csv', 'w', newline='') as csvfile:
+    propwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    propwriter.writerow(PROP_COL_KEYS)
+    for prop in propositions:
+      propwriter.writerow([prop[k] for k in PROP_COL_KEYS])
+
+def write_picks_csv(picks, filename):
+  with open(filename, 'w', newline='') as csvfile:
+    pickwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+    pickwriter.writerow(PICKS_COL_KEYS)
+    for pick in picks:
+      pickwriter.writerow([pick[k] for k in PICKS_COL_KEYS])
+
+def load_games_csv(filename):
+  games = []
+  with open(filename, newline='') as csvfile:
+    gamereader = csv.DictReader(csvfile, delimiter=',')
+    return [row for row in gamereader]
 
 if __name__ == "__main__":
-  # games = get_game_scores()
-  # write_games_csv(games)
-  # propositions = get_propositions()
-  # write_propositions_csv(propositions)
-  smb_picks = get_smb_picks()
-  write_picks_csv(smb_picks, 'smb_picks.csv')
+  propositions = get_propositions()
+  write_propositions_csv(propositions)
+  picks = {}
+  for (k, v) in PLAYER_IDS.items():
+    picks[k] = get_picks(v)
+    write_picks_csv(picks[k], f'{k}.csv')
+  games = get_game_scores()
+  write_games_csv(games, 'base_games.csv')
+  # games = load_games_csv('base_games.csv')
   
+  # Populate lines & picks. A faster, non n^2 way to do this would be to build
+  # an index but this dumb way is so much faster than the
+  # fetches that it doesn't matter.
+  for game in games:
+    for prop in propositions:
+      if game[GAME_ID_KEY] == prop[GAME_ID_KEY]:
+        game[LINE_KEY] = prop[LINE_KEY]
+        game[PROPOSITION_ID_KEY] = prop[PROPOSITION_ID_KEY]
+        game[CORRECT_OUTCOME_ABBREV_KEY] = prop[CORRECT_OUTCOME_ABBREV_KEY]
+        game[INCORRECT_OUTCOME_ABBREV_KEY] = prop[INCORRECT_OUTCOME_ABBREV_KEY]
+  for game in games:
+    for k in PLAYER_IDS.keys():
+      for pick in picks[k]:
+        if game[PROPOSITION_ID_KEY] == pick[PROPOSITION_ID_KEY]:
+          if(pick[RESULT_KEY]) == 'CORRECT':
+            game[k] = game[CORRECT_OUTCOME_ABBREV_KEY]
+          else:
+            game[k] = game[INCORRECT_OUTCOME_ABBREV_KEY]
+
+write_games_csv(games, 'games.csv')
